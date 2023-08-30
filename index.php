bc0614a1021e74a25cc4ddb59c7889964d911781
Veustyle <128892733+Veustyle@users.noreply.github.com>
2023-08-30 15:11:49 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="fr">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport"
+         content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+   <script src="game.js" type="module"></script>
+   <title>Bricks-Breaker</title>
+</head>
+<body style="padding: 0;margin: 0;">
+
+<canvas width="500" height="500"></canvas>
+
+</body>
+</html>
